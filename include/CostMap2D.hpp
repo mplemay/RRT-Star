305ac3dda72c1802ad8e71a8ec1bfdd414b39925
Matthew LeMay <mplemay@users.noreply.github.com>
2020-10-19 15:35:19 -0400

    Removed the include file from the gitignore

---
+++ include/CostMap2D.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include <map>
+
+/**
+ * A simple representation of a point in 2D space.
+ */
+typedef std::tuple<int64_t, int64_t> Point2D;
+
+class Costmap2D {
+public:
+    Costmap2D() noexcept = default;
+    std::optional<bool> update(const Point2D &point, const bool &value);
+    std::optional<bool> get(const Point2D &point) const;
+    [[nodiscard]] bool contains(const Point2D &point) const;
+    [[nodiscard]] std::tuple<std::uniform_int_distribution<>, std::uniform_int_distribution<>> sample_space(const std::vector<Point2D> &waypoints) const;
+
+private:
+    std::map<Point2D, bool> sparse_costmap;
+    std::optional<int64_t> min_x, max_x, min_y, max_y;
+};
